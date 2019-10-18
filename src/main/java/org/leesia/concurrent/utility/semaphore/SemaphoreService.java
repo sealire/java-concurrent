7f05802cd1e9d1032441b54b3f6710de47070123
wenjinbiao <wenjb@hxmec.com>
2019-10-18 17:21:56 +0800

    修改信号量

--- src/main/java/org/leesia/concurrent/utility/semaphore/SemaphoreService.java
+++ src/main/java/org/leesia/concurrent/utility/semaphore/SemaphoreService.java
@@ -1,22 +1,97 @@
 package org.leesia.concurrent.utility.semaphore;
 
 import java.util.concurrent.Semaphore;
+import java.util.function.Function;
 
 public class SemaphoreService {
 
-    private Semaphore semaphore = new Semaphore(1);
+    private Semaphore semaphore_1 = new Semaphore(1);
 
-    public void testMethod() {
-        try {
+    private Semaphore semaphore_2 = new Semaphore(2);
+
+    private Semaphore semaphore_10 = new Semaphore(10);
+
+    /**
+     * 1个信号量，每次竞争1个
+     *
+     * @param function
+     * @throws InterruptedException
+     */
+    public void syn1_1(Function function) throws InterruptedException {
+        acquire(semaphore_1, 1);
+
+        function.apply(null);
+
+        release(semaphore_1, 1);
+    }
+
+    /**
+     * 2个信号量，每次竞争1个
+     *
+     * @param function
+     * @throws InterruptedException
+     */
+    public void syn2_1(Function function) throws InterruptedException {
+        acquire(semaphore_2, 1);
+
+        function.apply(null);
+
+        release(semaphore_2, 1);
+    }
+
+    /**
+     * 10个信号量，每次竞争3个
+     * @param function
+     * @throws InterruptedException
+     */
+    public void syn10_3(Function function) throws InterruptedException {
+        acquire(semaphore_10, 3);
+
+        function.apply(null);
+
+        release(semaphore_10, 3);
+    }
+
+    /**
+     * 获取信号量
+     *
+     * @param semaphore
+     * @param permits
+     * @throws InterruptedException
+     */
+    public void acquire(Semaphore semaphore, int permits) throws InterruptedException {
+        if (permits < 2) {
             semaphore.acquire();
-            System.out.println(Thread.currentThread().getName()
-                    + " begin timer=" + System.currentTimeMillis());
-            Thread.sleep(5000);
-            System.out.println(Thread.currentThread().getName()
-                    + "   end timer=" + System.currentTimeMillis());
+        } else {
+            semaphore.acquire(permits);
+        }
+    }
+
+    /**
+     * 获取信号量，不可中断
+     *
+     * @param semaphore
+     * @param permits
+     */
+    public void acquireUninterruptibly(Semaphore semaphore, int permits) {
+        if (permits < 2) {
+            semaphore.acquireUninterruptibly();
+        } else {
+            semaphore.acquireUninterruptibly(permits);
+        }
+    }
+
+    /**
+     * 释放信号量
+     *
+     * @param semaphore
+     * @param permits
+     */
+    public void release(Semaphore semaphore, int permits) {
+        if (permits < 2) {
             semaphore.release();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } else {
+            semaphore.release(permits);
         }
     }
 }
--- src/test/java/org/leesia/concurrent/utility/semaphore/SemaphoreTest.java
+++ src/test/java/org/leesia/concurrent/utility/semaphore/SemaphoreTest.java
@@ -1,29 +1,107 @@
 package org.leesia.concurrent.utility.semaphore;
 
+import org.leesia.concurrent.thread.RunnableFactory;
+import org.leesia.concurrent.util.FunctionUtil;
+import org.leesia.concurrent.util.RandomUtil;
+
+import java.util.Set;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SemaphoreTest {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(SemaphoreTest.class);
+
+    private static SemaphoreService semaphoreService = new SemaphoreService();
+
     public static void main(String[] args) {
-        SemaphoreService service = new SemaphoreService();
-        Thread[] threads = new Thread[3];
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(new Run(service));
+        test1_1_interrupt_random();
+
+        LOGGER.info("main exit");
+    }
+
+    /**
+     * 5个线程竞争1个信号量
+     */
+    public static void test1_1() {
+        run(5, x ->  {
+            try {
+                semaphoreService.syn1_1(FunctionUtil.newSleepFunction(2000));
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread: {} interrupted", Thread.currentThread().getName());
+            }
+            return x;
+        });
+    }
+
+    /**
+     * 5个线程竞争1个信号量，随机中断一些线程
+     */
+    public static void test1_1_interrupt_random() {
+        int threadCount = 5;
+        Thread[] threads = run(threadCount, x ->  {
+            try {
+                semaphoreService.syn1_1(FunctionUtil.newSleepFunction(2000));
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread: {} interrupted", Thread.currentThread().getName());
+            }
+            return x;
+        });
+
+        int count = RandomUtil.randomInt(1, threadCount, true);
+        interrupt(threads, count);
+
+    }
+
+    /**
+     * 5个线程竞争2个信号量
+     */
+    public static void test2_1() {
+        run(5, x ->  {
+            try {
+                semaphoreService.syn2_1(FunctionUtil.newSleepFunction(2000));
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread: {} interrupted", Thread.currentThread().getName());
+            }
+            return x;
+        });
+    }
+
+    /**
+     * 10个线程竞争10个信号量，每个线程竞争3个信号量
+     */
+    public static void test10_3() {
+        run(10, x ->  {
+            try {
+                semaphoreService.syn10_3(FunctionUtil.newSleepFunction(2000));
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread: {} interrupted", Thread.currentThread().getName());
+            }
+            return x;
+        });
+    }
+
+    private static Thread[] run(int threadCount, Function function) {
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(RunnableFactory.newRunnable(function));
         }
 
-        for (int i = 0; i < threads.length; i++) {
-            threads[i].start();
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        return threads;
+    }
+
+    private static void interrupt(Thread[] threads, int count) {
+        Set<Integer> indexes = RandomUtil.randomSet(0, threads.length - 1, count);
+        for (Integer index : indexes) {
+            threads[index].interrupt();
+            LOGGER.info("interrupt thread: {} ", Thread.currentThread().getName());
         }
     }
 }
 
-class Run implements Runnable {
-
-    private SemaphoreService service;
-
-    public Run(SemaphoreService service) {
-        this.service = service;
-    }
-
-    public void run() {
-        service.testMethod();
-    }
-}
